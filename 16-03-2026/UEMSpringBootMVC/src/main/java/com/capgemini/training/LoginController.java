c6354e885381d0ab40a5dd993d03b069f2b8a93e
Arju Mukhopadhyay <108166921+Arju-Mukhopadhyay@users.noreply.github.com>
2026-03-16 16:15:50 +0530

    CRUD Operations in the App

---
+++ 16-03-2026/UEMSpringBootMVC/src/main/java/com/capgemini/training/LoginController.java
@@ -0,0 +1,99 @@
+package com.capgemini.training;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    LoginServices loginServices;
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "login.jsp";
+    }
+
+    @PostMapping("/login")
+    public String validate(HttpServletRequest request) {
+    	String page=null;
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println(username);
+        System.out.println(password);
+
+        boolean flag = loginServices.validateUser(username, password);
+        if(flag) {
+        	request.setAttribute("message" , " <font style='color:green'>Logged in Successfully </font>");
+        	page="home.jsp";
+        }else {
+        	request.setAttribute("message" , " <font style='color:red'> Invalid user id or password </font>");
+        	page="login.jsp";
+        }
+        return page;
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register.jsp";
+    }
+
+    @PostMapping("/register")
+    public String register(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(username);
+        System.out.println(password);
+
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setName(name);
+        userEntity.setEmail(email);
+        userEntity.setUser(username);
+        userEntity.setPassword(password);
+
+        loginServices.registerUser(userEntity);
+        
+        boolean flag = loginServices.validateUser(username, password);
+        if(flag) {
+        	request.setAttribute("message" , " <font style='color:green'>Registered Successfully </font>");
+        
+        }
+
+        return "home.jsp";
+    }
+
+    @GetMapping("/forgetpassword")
+    public String forgotPage() {
+        return "forgetpassword.jsp";
+    }
+
+    @PostMapping("/forgetpassword")
+    public String forgot(HttpServletRequest request) {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        boolean flag = loginServices.updatePassword(username, password);
+
+        if(flag) {
+            request.setAttribute("message", "<font style='color:green'>Password Reset Successfully</font>");
+        }
+        else {
+            request.setAttribute("message", "<font style='color:red'>User not found</font>");
+        }
+
+        return "login.jsp";
+    }
+}
