c06ac949dbd9ec4f37ed620ab59b9936b32b448f
Arju Mukhopadhyay <108166921+Arju-Mukhopadhyay@users.noreply.github.com>
2026-03-17 21:36:36 +0530

    Delete UEMSpringBootMVC directory

--- UEMSpringBootMVC/27-02-2026/User.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-import jakarta.persistence.*;
-import jakarta.util.List;
-
-@Entity
-
-public class User {
-}
--- UEMSpringBootMVC/src/main/java/com/capgemini/training/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.training.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.training.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-	
-}
